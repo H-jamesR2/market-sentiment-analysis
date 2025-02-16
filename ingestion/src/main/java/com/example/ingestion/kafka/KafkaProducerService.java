595bd20ca0e1d7a9cfbdbc1a66dc3e2a10627c98
James.R <hilarionjames25@gmail.com>
2025-02-15 23:32:19 -0500

    added starter ingestion files w/ redditAPI starter implementations

---
+++ ingestion/src/main/java/com/example/ingestion/kafka/KafkaProducerService.java
@@ -0,0 +1,5 @@
+package main.java.com.example.ingestion.kafka;
+
+public class KafkaProducerService {
+    
+}
---
+++ ingestion/src/main/java/com/example/ingestion/reddit/RedditClient.java
@@ -0,0 +1,5 @@
+package main.java.com.example.ingestion.reddit;
+
+public class RedditClient {
+    
+}
