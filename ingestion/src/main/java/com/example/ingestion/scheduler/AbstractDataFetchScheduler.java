bcabcb17dbf46ee4307b9f9e3ec0d574a4625cbd
James.R <hilarionjames25@gmail.com>
2025-02-19 18:43:10 -0500

    modified schedulers

--- ingestion/src/main/java/com/example/ingestion/scheduler/AbstractDataFetchScheduler.java
+++ ingestion/src/main/java/com/example/ingestion/scheduler/AbstractDataFetchScheduler.java
@@ -1,15 +1,22 @@
 package com.example.ingestion.scheduler;
 
+import com.example.ingestion.fetcher.DataFetcher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import java.util.List;
 
-public abstract class AbstractDataFetchScheduler implements FetchScheduler {
+public abstract class AbstractDataFetchScheduler {
     private static final Logger logger = LoggerFactory.getLogger(AbstractDataFetchScheduler.class);
+    private final List<DataFetcher> fetchers;
 
-    @Scheduled(fixedRateString = "${fetch.scheduler.interval:60000}") // 60s interval
+    protected AbstractDataFetchScheduler(List<DataFetcher> fetchers) {
+        this.fetchers = fetchers;
+    }
+
+    @Scheduled(fixedRateString = "${fetch.scheduler.interval:120000}") // Default 60s interval
     public void scheduleFetch() {
         logger.info("Scheduled fetch triggered for: " + this.getClass().getSimpleName());
-        fetchData();
+        fetchers.forEach(DataFetcher::fetchData);
     }
 }
--- ingestion/src/main/java/com/example/ingestion/scheduler/FetchScheduler.java
+++
@@ -1,5 +0,0 @@
-package com.example.ingestion.scheduler;
-
-public interface FetchScheduler {
-    void fetchData(); // Defines the contract for fetching data
-}
